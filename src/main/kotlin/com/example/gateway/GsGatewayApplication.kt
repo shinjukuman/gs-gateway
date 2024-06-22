cc75e42b0745ebe1f29e6d1e9d495eff6259da8b
Cho Jeongho[ 曺正鎬 ] <jeongho.cho@linecorp.com>
2024-06-22 23:20:26 +0900

    Refactor annotation

--- src/main/kotlin/com/example/gateway/GsGatewayApplication.kt
+++ src/main/kotlin/com/example/gateway/GsGatewayApplication.kt
@@ -1,11 +1,8 @@
 package com.example.gateway
 
-import com.example.gateway.properties.UriProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@EnableConfigurationProperties(UriProperties::class)
 @SpringBootApplication
 class GsGatewayApplication
 
--- src/main/kotlin/com/example/gateway/routes/RouteConfiguration.kt
+++ src/main/kotlin/com/example/gateway/routes/RouteConfiguration.kt
@@ -1,12 +1,14 @@
 package com.example.gateway.routes
 
 import com.example.gateway.properties.UriProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableConfigurationProperties(UriProperties::class)
 class RouteConfiguration {
     @Bean
     fun myRoutes(builder: RouteLocatorBuilder, uriProperties: UriProperties): RouteLocator {

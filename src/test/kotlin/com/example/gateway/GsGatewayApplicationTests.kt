280b33db22f94e8da83fc36ccf674b05ac538b43
Cho Jeongho[ 曺正鎬 ] <jeongho.cho@linecorp.com>
2024-06-22 22:57:02 +0900

    Write tests

--- src/test/kotlin/com/example/gateway/GsGatewayApplicationTests.kt
+++ src/test/kotlin/com/example/gateway/GsGatewayApplicationTests.kt
@@ -1,13 +1,81 @@
 package com.example.gateway
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GsGatewayApplicationTests {
+    @Autowired
+    private lateinit var webClient: WebTestClient
 
     @Test
+    @Throws(Exception::class)
     fun contextLoads() {
+        webClient
+            .get().uri("/get")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+            .jsonPath("$.headers.Hello").isEqualTo("World")
+
+        webClient
+            .get().uri("/delay/3")
+            .header("Host", "www.circuitbreaker.com")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+            .consumeWith { response ->
+                assertThat(response.responseBody).isEqualTo("fallback".toByteArray())
+            }
     }
 
+    companion object {
+        private lateinit var wireMockServer: WireMockServer
+
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            wireMockServer = WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+            wireMockServer.start()
+
+            wireMockServer.stubFor(
+                get(urlEqualTo("/get"))
+                    .willReturn(
+                        aResponse()
+                            .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+                            .withHeader("Content-Type", "application/json")
+                    )
+            )
+            wireMockServer.stubFor(
+                get(urlEqualTo("/delay/3"))
+                    .willReturn(
+                        aResponse()
+                            .withBody("no fallback")
+                            .withFixedDelay(3000)
+                    )
+            )
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun teardown() {
+            wireMockServer.stop()
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun dynamicProperties(registry: DynamicPropertyRegistry) {
+            registry.add("uri.httpbin") { "http://localhost:${wireMockServer.port()}" }
+        }
+    }
 }

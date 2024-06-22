f9797086f805c983432d42f83124fdb5dd28bb12
Cho Jeongho[ 曺正鎬 ] <jeongho.cho@linecorp.com>
2024-06-22 18:05:18 +0900

    Fix routes

--- src/main/kotlin/com/example/gateway/GsGatewayApplication.kt
+++ src/main/kotlin/com/example/gateway/GsGatewayApplication.kt
@@ -2,9 +2,24 @@ package com.example.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class GsGatewayApplication
+class GsGatewayApplication {
+    @Bean
+    fun myRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        return builder
+            .routes()
+            .route("httpbin_route") { p ->
+                p.path("/get")
+                    .filters { f -> f.addRequestHeader("hello", "world") }
+                    .uri("http://httpbin.org:80")
+            }
+            .build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<GsGatewayApplication>(*args)
